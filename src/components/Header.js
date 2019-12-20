c5bf9e41aa9e18faae4a02a800853ce1ebaf0d21
debauchery1st <trevor.j.martin@gmail.com>
2019-12-20 11:40:51 -0500

    started WelcomePage & updated libraries

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,19 @@
 import React from "react";
+import styled from 'styled-components';
+
+const H1 = styled.h1`
+  display: block;
+  font-family: 'Letter Gothic', sans-serif;
+  font-weight: 1000;
+  letter-spacing: -.1rem;
+  text-transform: uppercase;
+  word-spaceing: -.3rem;
+`;
 
 export default function Header() {
   return (
     <header className="ui centered">
-      <h1 className="ui center">Rick &amp; Morty Fan Page</h1>
+      <H1 className="ui center">Rick &amp; Morty Fan Page</H1>
     </header>
   );
 }
