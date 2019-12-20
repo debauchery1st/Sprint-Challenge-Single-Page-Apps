fdddda64a8bed358b17cc7853887ba91fd75ef77
debauchery1st <trevor.j.martin@gmail.com>
2019-12-20 17:22:31 -0500

    added image to CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,11 +5,19 @@ const UL = styled.ul`
 `;
 const LI = styled.li`
 `;
+const IMG = styled.img`
+  box-shadow: 0 19px 38px rgba(0, 0, 0, 0.3), 0 15px 12px rgba(0, 0, 0, 0.22);
+  :hover {
+    box-shadow: 0 9px 18px rgba(0, 0, 0, 0.3), 0 7px 6px rgba(0, 0, 0, 0.22);
+  }
+`;
 
 export default function CharacterCard(props) {
   const cardInfo = props.cardInfo;
   console.log(Object.keys(cardInfo))
   const showUser = [cardInfo.name, cardInfo.species, cardInfo.status];
-
-  return <UL>{showUser.map((info) => <LI>{info}</LI>)}</UL>;
+  return <UL>
+    <IMG src={cardInfo.image}></IMG>
+    {showUser.map((info) => <LI>{info}</LI>)}
+    </UL>;
 }
